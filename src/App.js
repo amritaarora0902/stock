423a56576f738b8b204705a0fa121fd1456f848e
amrita <amritaarora2424@gmail.com>
2020-01-27 11:52:11 +0530

    changedback

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 // import Stock from './components/stock';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Back from './components/background';
-// import Users from './components/users';
+import Users from './components/users';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Back />
-      {/* <Users /> */}
+      
+      <Users />
     </div>
   );
 }
